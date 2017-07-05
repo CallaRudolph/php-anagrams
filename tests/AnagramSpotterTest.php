76640984685638907e7741c2a2642da5f40ba289
Calla Rudolph and Nathan Stewart <nathan@>
2017-07-05 10:04:06 -0700

    refactor/remove redundant test

--- tests/AnagramSpotterTest.php
+++ tests/AnagramSpotterTest.php
@@ -17,20 +17,6 @@
             $this->assertEquals("Please do not enter numbers or special characters", $result);
         }
 
-        function test_checkForAnagrams_lowerCase()
-        {
-            //Arrange
-            $test_AnagramSpotter = new AnagramSpotter;
-            $input_word = "Pail";
-            $input_check_word = "carroT";
-
-            //Act
-            $result = $test_AnagramSpotter->checkForAnagrams($input_word, $input_check_word);
-
-            //Assert
-            $this->assertEquals(["pail", "carrot"], $result);
-        }
-
         function test_checkForAnagrams_lowerCaseAll()
         {
             //Arrange
