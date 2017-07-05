36d32461ee61916f81e231c8dc27b26d12ed689b
Calla Rudolph and Nathan Stewart <nathan@>
2017-07-05 09:29:17 -0700

    add test for single word to check and fail

--- tests/AnagramSpotterTest.php
+++ tests/AnagramSpotterTest.php
@@ -15,5 +15,19 @@
             //Assert
             $this->assertEquals("Please do not enter numbers or special characters", $result);
         }
+
+        function test_checkForAnagrams_singleWord()
+        {
+            //Arrange
+            $test_AnagramSpotter = new AnagramSpotter;
+            $input_word = "pail";
+            $input_checkWord = "carrot";
+
+            //Act
+            $result = $test_AnagramSpotter->checkForAnagrams($input_word, $input_checkWord);
+
+            //Assert
+            $this->assertEquals("There are no possible anagrams", $result);
+        }
     }
 ?>
